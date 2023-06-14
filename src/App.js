c64e28c871e3a66a92091b5a3bccc1e03b0785f0
deepika <g.deepikareddy7@gmail.com>
2023-06-14 21:07:24 +0530

    project added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <h1>This line is added by Manjunadh from Github.com</h1>
       <h1>This second line is added by Manjunadh from local repository</h1>
       <h1>This third line is added by Veera</h1>
+      <h1>This line is added by Deepika</h1>
     </div>
   );
 }
